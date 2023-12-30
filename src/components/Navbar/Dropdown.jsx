187fccfd091c6a76e95b54143203ae75f62511de
krishna soni <soni01krishna@gmail.com>
2023-12-30 13:03:52 +0530

    add link to account li in dropdown

--- src/components/Navbar/Dropdown.jsx
+++ src/components/Navbar/Dropdown.jsx
@@ -27,7 +27,7 @@ const Dropdown = ( )=>{
     return <>
     <div className="h-40 w-64 bg-white shadow-md py-3 rounded-lg border">
         <ul>
-            <li className=" px-3 "><Link className=" hover:bg-gray-100 flex items-center justify-center text-lg gap-x-2 p-2 rounded-lg active:scale-95 duration-200"><LuUser2/> Account</Link></li>
+            <li onClick={()=> navi('/account')} className=" px-3 "><Link className=" hover:bg-gray-100 flex items-center justify-center text-lg gap-x-2 p-2 rounded-lg active:scale-95 duration-200"><LuUser2/> Account</Link></li>
             <li className="px-3 pt-5"><Link className="hover:bg-red-200 flex items-center justify-center text-lg gap-x-2 p-2 rounded-lg active:scale-95 duration-200" onClick={logout}><LuLogOut/>Logout</Link></li>
         </ul>
     </div>
