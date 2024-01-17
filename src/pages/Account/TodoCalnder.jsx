2ad47d2a6f5780d2bef780be6f2ae176f74dc19b
krishna soni <soni01krishna@gmail.com>
2024-01-17 19:04:43 +0530

    working on the attandence in place of calender

--- src/pages/Account/TodoCalnder.jsx
+++ src/pages/Account/TodoCalnder.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
-import Calendar from 'react-calendar';
+
 export default function TodoCalnder() {
   return (
     <div className='font-1'>
-      <Calendar />
-    
+      
     </div>
   )
 }
